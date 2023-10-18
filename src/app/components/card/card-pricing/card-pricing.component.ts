49bc19ca786048a0b7e4b066c1822f824dcd0740
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-10-18 15:57:33 +0000

    criando sub components para o card principal

---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css']
+})
+export class CardPricingComponent {
+
+}
