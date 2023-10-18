3afd240d0c0b388199bb476353bf73dfee6e4fdd
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-10-18 20:26:16 +0000

    projeto base finalizado

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
   templateUrl: './card-pricing.component.html',
   styleUrls: ['./card-pricing.component.css']
 })
-export class CardPricingComponent {
+export class CardPricingComponent implements OnInit {
 
+  @Input()
+  gameType: string = "Digital PS4"
+  @Input()
+  gamePrice: string = "R$ 399,90"
+
+  constructor () {}
+
+  ngOnInit(): void {
+    
+  }
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
 
+  @Input()
+  gameCover: string = ""
+  @Input()
+  gameLabel: string = ""
+  @Input()
+  gameType: string = "Digital PS4"
+  @Input()
+  gamePrice: string = "R$ 399,90"
+
+
+  constructor () {}
+
+  ngOnInit(): void {
+    
+  }
 }
